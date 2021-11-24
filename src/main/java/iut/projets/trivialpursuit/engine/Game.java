865d743163df79382129e70fc37dce5a89dc52e3
Clement-GRASSET <clement.grasset@etu.univ-amu.fr>
2021-11-24 23:19:09 +0100

    Nettoyage

--- src/main/java/iut/projets/trivialpursuit/engine/Game.java
+++ src/main/java/iut/projets/trivialpursuit/engine/Game.java
@@ -4,23 +4,9 @@ import iut.projets.trivialpursuit.engine.graphics.Scene;
 
 public abstract class Game {
 
-    private Scene activeScene;
-
-    public Game() {
-        activeScene = new Scene();
-    }
-
     public abstract void start();
 
     protected void setName(String name) {
         Engine.getGameWindow().setTitle(name);
-    }
-
-    public Scene getActiveScene() {
-        return activeScene;
-    }
-
-    public void setActiveScene(Scene activeScene) {
-        this.activeScene = activeScene;
     }
 }
